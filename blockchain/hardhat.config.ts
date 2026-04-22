7b4833b4636af7b530f43871c56c99c5434f835d
badisso <127608561+badisso22@users.noreply.github.com>
2026-04-22 19:56:56 +0100

    Update hardhat.config.ts

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "hardhat/config";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export default defineConfig({
   solidity: {
     version: "0.8.28",
+  },
+  networks: {
+    sepolia: {
+      type: "http",
+      url: process.env.ALCHEMY_RPC_URL ?? "",
+      accounts: process.env.PRIVATE_KEY 
+        ? [process.env.PRIVATE_KEY] 
+        : [],
+    },
   },
 });
